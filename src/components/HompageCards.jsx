f22247156ee41d1a03395c10de2da96a0197fe7a
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-03 10:50:03 +0200

    .

---
+++ src/components/HompageCards.jsx
@@ -0,0 +1,21 @@
+export default function HomepageCards() {
+  return (
+    <>
+      <section className="title">
+        <div className="content">
+          <h1></h1>
+          <h3>-Multimediedesigner og webudvikler</h3>
+        </div>
+      </section>
+
+      <section className="cardContainer">
+        <a href="design.html">
+          <div className="card1">
+            <h2>Design</h2>
+            <img src="images/Placeholders/DesignPlaceholder.svg" alt="" />
+          </div>
+        </a>
+      </section>
+    </>
+  );
+}
